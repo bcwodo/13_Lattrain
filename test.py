c2feca64eb31af33949e6f482af09146a7f1e80a
Bernd Christian <Bernd.Christian@Kantar.com>
2022-12-09 12:36:10 +0100

    wiederholung fehlt noch

--- test.py
+++ test.py
@@ -4,8 +4,5 @@ import numpy as np
 df = pd.read_csv("vokabeln.csv")
 #df["random"] = np.random.randn(df.shape[0])
 #df.sort_values(["Kartei", "random"], ascending=False, inplace=True )
-t = df["Deutsch"].values[173]
-if len(t)>80:
-    x = t.split(";")
-    for i in x:
-        print(i.strip(" "))
+kartei = df.Kartei.value_counts()
+print
